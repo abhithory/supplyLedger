80414355edfd624c0828ce12652f979417124073
theteamisbest <abhithory@gmail.com>
2023-06-03 14:35:39 +0530

    project intialiez with smart contract

---
+++ smart contract/contracts/SupplyLeger.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+
+contract SupplyLeger {
+    uint public unlockTime;
+    address payable public owner;
+
+    event Withdrawal(uint amount, uint when);
+
+    constructor(uint _unlockTime) payable {
+        require(
+            block.timestamp < _unlockTime,
+            "Unlock time should be in the future"
+        );
+
+        unlockTime = _unlockTime;
+        owner = payable(msg.sender);
+    }
+
+}
