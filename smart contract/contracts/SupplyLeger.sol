793af4d40b0040d464c2c78a4c7680d63116e905
theteamisbest <abhithory@gmail.com>
2023-06-03 20:42:13 +0530

    basic farm and colloctor contract created

--- smart contract/contracts/SupplyLeger.sol
+++ smart contract/contracts/SupplyLeger.sol
@@ -2,20 +2,48 @@
 pragma solidity ^0.8.9;
 
 // import "hardhat/console.sol";
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-
+// console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
 
 contract Farm {
+    string public id;
     string public name;
-    // address
-    string public farmId;
+    string public location;
 
+    struct FarmItem {
+        string quality;
+    }
+
+    mapping(uint256 => FarmItem) public itemDetailFromFarm;
+
+    constructor(string memory _id) {
+        id = _id;
+    }
+}
+
+contract LocalCollector {
+    string public id;
+    string public name;
+    string public location;
+
+    struct ItemDetail {
+        uint256 reachedAt;
+        uint256 dispatchedAt;
+        string quality;
+    }
+
+    mapping(uint256 => ItemDetail) public itemDetailFromFarm;
+
+    constructor(string memory _id) {
+        id = _id;
+    }
+}
+
+contract RetailStore {
+    string public name;
+    string public id;
 }
 
 contract SupplyLeger {
-    // event Withdrawal(uint amount, uint when);
-
     struct FoodItem {
         uint256 id;
         string name;
@@ -27,23 +55,23 @@ contract SupplyLeger {
         // address distributors;
     }
 
-    struct ItemTrackDetail{
+    struct ItemTrackDetail {
         string checkPoint;
         uint256 time;
         // uint256 reachedTime;
         // uint256 dispachedTime;
     }
     uint256 foodItemId;
-    mapping (uint256=>FoodItem) public foodItems;
-    mapping (uint256=>ItemTrackDetail[]) public getAllTracks;
+    mapping(uint256 => FoodItem) public foodItems;
+    mapping(uint256 => ItemTrackDetail[]) public getAllTracks;
 
-    constructor() {
-
-    }
-
+    constructor() {}
 
     // food item dispatched from farm to local colloctor
-    function dispachedToLocalColloctor(address _farm, address _localColloctor) public{   
+    function dispachedToLocalColloctor(
+        address _farm,
+        address _localColloctor
+    ) public {
         foodItems[foodItemId] = FoodItem(
             foodItemId,
             "Potato",
@@ -53,20 +81,37 @@ contract SupplyLeger {
             0
         );
 
-        getAllTracks[foodItemId].push(ItemTrackDetail("Dispactched From Farm to local store", block.timestamp));
+        getAllTracks[foodItemId].push(
+            ItemTrackDetail(
+                "Dispactched From Farm to local store",
+                block.timestamp
+            )
+        );
         foodItemId++;
     }
 
-
     // food item dispatched from local Collortor to retail store
-    function dispachedToRetailStore(uint256 _itemId, address _retailStore) public {
-        getAllTracks[_itemId].push(ItemTrackDetail("Dispactched From local store to retail store", block.timestamp));
+    function dispachedToRetailStore(
+        uint256 _itemId,
+        address _retailStore
+    ) public {
+        getAllTracks[_itemId].push(
+            ItemTrackDetail(
+                "Dispactched From local store to retail store",
+                block.timestamp
+            )
+        );
         foodItems[_itemId].retailStore = _retailStore;
     }
 
     // item purcased
-  function itemPurchased(uint256 _itemId) public {
-        getAllTracks[_itemId].push(ItemTrackDetail("Item sold from Retail store", block.timestamp));
+    function itemPurchased(uint256 _itemId) public {
+        getAllTracks[_itemId].push(
+            ItemTrackDetail("Item sold from Retail store", block.timestamp)
+        );
         foodItems[_itemId].purchasedDate = block.timestamp;
     }
+
+    // update
+    // function
 }
